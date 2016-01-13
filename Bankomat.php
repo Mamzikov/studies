d38e29845b6c5dfb269c7472bea6a1108f8ef4f6
Aleksandr Mamzikov <al.mamzikov@gmail.com>
2016-01-13 21:48:20 +0400

    Finished with bankomat output monitor.

--- Bankomat.php
+++ Bankomat.php
@@ -6,51 +6,125 @@
  * Time: 10:06
  */
 error_reporting(-1);
-mb_internal_encoding('utf-8');
+ mb_internal_encoding('utf-8');
 
+include 'main_coiffured.php';
 
-function textNumber ($amount) {
-    include 'main_coiffured.php';
+function textNumber($amount)
+{
     return bigNumberToText($amount);
 }
 
-function numberOfBills ($amount, $bills) {
+function numberOfBills($amount, $bills)
+{
 
-    if ($amount%100 !=0) {
-        echo 'Сумма '.$amount.' не кратна 100, введите корректную сумму';
-        exit;
-    }
-    $result = 'Доступны к получению купюры: ';
-    $restSum = $amount;
+    if ($amount % 100 != 0) {
+        $result = 'Сумма ' . $amount . ' не кратна 100, введите корректную сумму';
+    } elseif ($amount == 0) {
+        $result = 'На счете нет доступной для сния суммы';
+    } else {
 
-    foreach ($bills as $nominal => $restBills ) {
-        $sumBills =0;
-        while (($restSum >= $nominal) and ($sumBills <=$restBills)) {
-            $restSum = $restSum - $nominal;
-            $restBills--;
-            $sumBills++;
+        $result = (array_sum($bills) != 0) ? 'Доступны к получению купюры: ' : '';
+
+        $restSum = $amount;
+
+        foreach ($bills as $nominal => $restBills) {
+            $sumBills = 0;
+            while (($restSum >= $nominal) and ($restBills > 0)) {
+
+                $restSum = $restSum - $nominal;
+                $restBills--;
+                $sumBills++;
+
+            }
+
+            if ($sumBills != 0) $result .= $sumBills . 'шт. по ' . $nominal . 'руб. ';
         }
-        if ($sumBills !=0) $result .= $sumBills.'шт. по '.$nominal.'руб. ';
-    }
+        if ($restSum != 0) $result .= 'Остаток ' . $restSum . 'руб. не может быть выдан из-за отстутвия в банкомате необходимого количества банкнот';
 
+    }
+    return $result;
+}
+
+function numberOfBills2($amount, $bills) {
+
+    if ($amount % 100 != 0) {
+        $result = 'Сумма ' . $amount . ' не кратна 100, введите корректную сумму';
+    } elseif ($amount == 0) {
+        $result = 'На счете нет доступной для сния суммы';
+    } else {
+
+        $result = (array_sum($bills) != 0) ? 'Доступны к получению купюры: ' : '';
+
+        $restSum = $amount;
+
+        foreach ($bills as $nominal => $restBills) {
+
+            $totalBills = floor($restSum / $nominal);
+
+            if ($totalBills >= $restBills) {
+                $sumBills = $restBills;
+            } else {
+                $sumBills = $totalBills;
+            }
+            $restSum = $restSum - $nominal * $sumBills;
+
+            if ($sumBills != 0)
+                $result .= $sumBills . 'шт. по ' . $nominal . 'руб. ';
+        }
+        if ($restSum != 0)
+            $result .= 'Остаток ' . $restSum . 'руб. не может быть выдан из-за отстутвия в банкомате необходимого количества банкнот';
+    }
     return $result;
 }
 
 
+// 0.85304880142212
+//9.0505170822144
 
-function outputTextOnMonitor ($amount, $bills) {
-    echo 'Сумма: ('.$amount.')'.textNumber($amount).PHP_EOL;
-    echo numberOfBills ($amount, $bills).PHP_EOL;
+function comparisonOfWorkingTime()
+{
+    $t = -microtime(true);
+    for ($q = 0; $q < 100000; ++$q) {
+        $res = numberOfBills2(50232300, array(5000 => 1897, 1000 => 222, 500 => 0, 100 => 500000,));
+    }
+    $t += microtime(true);
+    echo $t, PHP_EOL;
+
+    $t = -microtime(true);
+    for ($q = 0; $q < 100000; ++$q) {
+        $res = numberOfBills2(50232300, array(5000 => 0, 1000 => 0, 500 => 0, 100 => 500000,));
+    }
+    $t += microtime(true);
+    echo $t, PHP_EOL;
 }
 
-outputTextOnMonitor (53400, array(5000 => 20, 1000 => 6, 500 => 15, 100 => 40,));
+//comparisonOfWorkingTime();
+//exit;
 
-function testNumber($amount, $bills, $validResult) {
-    if (outputTextOnMonitor($amount, $bills) == $validResult) {
-        echo 'Ошибок нет!'.PHP_EOL.outputTextOnMonitor ($amount, $bills).PHP_EOL;
+
+function outputTextOnMonitor($amount, $bills)
+{
+    echo 'Сумма: (' . $amount . ')' . textNumber($amount) . PHP_EOL;
+    echo numberOfBills($amount, $bills) . PHP_EOL;
+}
+
+
+function testNumber($amount, $bills, $validResult)
+{
+    if (numberOfBills2($amount, $bills) == $validResult) {
+        echo 'Ошибок нет! ' . numberOfBills2($amount, $bills) . PHP_EOL;
     } else {
-        echo 'Ошибка! Текст на экране:'.PHP_EOL.outputTextOnMonitor ($amount, $bills).PHP_EOL.'Ожидается:'.PHP_EOL.$validResult.PHP_EOL;
+        echo 'Ошибка! Текст на экране:' . PHP_EOL . numberOfBills2($amount, $bills) . PHP_EOL . 'Ожидается:' . PHP_EOL . $validResult . PHP_EOL;
     }
 }
 
-//testNumber (53400, array(5000 => 15, 1000 => 6, 500 => 15, 100 => 40,), 'Сумма: (53400) пятьдесят три тысячи четыреста рублей 0 копеекДоступны к получению купюры: 10шт. по 5000руб. 3шт. по 1000руб. 4шт. по 100руб.');
+testNumber(53400, array(5000 => 15, 1000 => 6, 500 => 15, 100 => 40,), 'Доступны к получению купюры: 10шт. по 5000руб. 3шт. по 1000руб. 4шт. по 100руб. ');
+testNumber(53400, array(5000 => 0, 1000 => 6, 500 => 15, 100 => 400,), 'Доступны к получению купюры: 6шт. по 1000руб. 15шт. по 500руб. 399шт. по 100руб. ');
+testNumber(53400, array(5000 => 15, 1000 => 6, 500 => 15, 100 => 0,), 'Доступны к получению купюры: 10шт. по 5000руб. 3шт. по 1000руб. Остаток 400руб. не может быть выдан из-за отстутвия в банкомате необходимого количества банкнот');
+testNumber(53400, array(5000 => 15, 1000 => 0, 500 => 15, 100 => 0,), 'Доступны к получению купюры: 10шт. по 5000руб. 6шт. по 500руб. Остаток 400руб. не может быть выдан из-за отстутвия в банкомате необходимого количества банкнот');
+testNumber(53400, array(5000 => 15, 1000 => 0, 500 => 0, 100 => 0,), 'Доступны к получению купюры: 10шт. по 5000руб. Остаток 3400руб. не может быть выдан из-за отстутвия в банкомате необходимого количества банкнот');
+testNumber(53400, array(5000 => 0, 1000 => 0, 500 => 0, 100 => 0,), 'Остаток 53400руб. не может быть выдан из-за отстутвия в банкомате необходимого количества банкнот');
+testNumber(0, array(5000 => 0, 1000 => 2, 500 => 0, 100 => 0,), 'На счете нет доступной для сния суммы');
+testNumber(3410, array(5000 => 0, 1000 => 2, 500 => 0, 100 => 0,), 'Сумма 3410 не кратна 100, введите корректную сумму');
+//testNumber (1000, array(5000 => 0, 1000 => 0, 500 => 0, 100 => 0,), 'Сумма 3410 не кратна 100, введите корректную сумму');
